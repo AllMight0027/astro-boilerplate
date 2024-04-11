1caa4cccd2adeffe7e8c305f3484fc22b44669bf
Pratyaksh Saxena <pratyakshsaxena88@gmail.com>
2024-04-11 22:29:00 +0530

    lint added

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,9 @@
-module.exports = {
+export default {
   // Type check TypeScript files
   "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
 
   // Lint then format TypeScript and JavaScript files
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix ${filenames.join(" ")}`,
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
 
