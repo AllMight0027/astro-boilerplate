116e78f5c64db9f9ff5dbe802d7a8170d53bf298
Pratyaksh Saxena <pratyakshsaxena88@gmail.com>
2024-04-18 10:50:49 +0530

    fix

--- src/stores/ratingMeters/actions.ts
+++ src/stores/ratingMeters/actions.ts
@@ -1,17 +1,8 @@
 import type { TableDataProps } from "../../atoms/TableData/TableData";
-import useRatingMeters, { type RMStoreProps } from ".";
 import { modifyRatingMetersList } from "../../utils/ratingMeterUtil";
 import { ratingMeterList } from "../../../fakeData";
-import type { DeepMapStore } from "nanostores";
 import { ratingMeterModifier } from "./modifier";
 import { ADD_RATING_METER, DELETE_RATING_METER } from "./types";
-
-export function setRatingMeters(
-  $store: DeepMapStore<RMStoreProps>,
-  rows: TableDataProps[][],
-) {
-  // $ratingMeterStore?.setKey("rows", rows);
-}
 
 export function fetchRatingMeters() {
   const rows: TableDataProps[][] = modifyRatingMetersList(ratingMeterList);
